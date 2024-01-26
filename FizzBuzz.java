0dd0ef5528a281bcb10c5b0d7a685f3686b6fbc9
Adlin Louisama <louisama.adlin@courrier.uqam.ca>
2024-01-26 14:41:22 -0500

    modification FizzBuzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,6 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        for(int i= 0; i<= 100; i++){
+        for(int i= 0; i<= 200; i++){
             if(i % 3 == 0 || i % 5 == 0 ){
                 System.out.println("FizzBuzz");
 
